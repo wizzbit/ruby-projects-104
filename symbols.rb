66cfa1cea6ecf884f46996dede7b20b59bb07fdd
Wizzbit <wizzbarclay@gmail.com>
2016-05-18 11:16:41 +0100

    second commit

--- symbols.rb
+++ symbols.rb
@@ -1,10 +1,10 @@
  	
-hash_01 = { name: “Kelly”, email: “kelly.wonderful@gmail.com”}
-hash_02 = { name: “James”, email: “james.wonderful@gmail.com”} 	
-hash_03 = { name: “Barry”, email: “barry.wonderful@gmail.com”} 	
-hash_04 = { name: “Elizabeth”, email: “lizz.wonderful@gmail.com”}  	
-hash_05 = { name: “Laura”, email: “laura.wonderful@gmail.com”}
+hash_01 = { name:  'Kelly', email:  'kelly.wonderful@gmail.com'}
+hash_02 = { name:  'James', email:  'james.wonderful@gmail.com'} 	
+hash_03 = { name:  'Barry', email: 'barry.wonderful@gmail.com'} 	
+hash_04 = { name: 'Elizabeth', email: 'lizz.wonderful@gmail.com'}  	
+hash_05 = { name:  'Laura', email: 'laura.wonderful@gmail.com'}
 
 winner = hash_04[:name]
 
-pust "You are the winnder of a brand new apple mac pro!"
+puts "You are the winner of a brand new apple mac pro!"
